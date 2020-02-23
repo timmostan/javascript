8a32cc4eb7ed7ae6452728c74d9ba44ef8ab8adc
timmostan <timmo@poczta.fm>
2020-02-23 11:55:51 +0100

    Styling + passing methods as props

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,44 +1,74 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-const App = props => {
-
-  const [ personsState, setPersonsState ] = useState({
+class App extends Component {
+  state = {
     persons: [
       { name: 'Stan', age: 36 },
       { name: 'Ola', age: 35 },
       { name: 'Pams', age: 7 }
     ],
-    otherState: 'some other values'
-  });
+    otherState: ''
+  }
 
-  const switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     //console.log('Was clicked');
     //DONT DO THIS!: this.state.persons[0].name = 'Stanislaw';
-    setPersonsState({
+    this.setState({
       persons: [
-        { name: 'Stanislaw', age: 36 },
+        { name: newName, age: 36 },
         { name: 'Olcia', age: 35 },
         { name: 'Pamirek', age: 7 }
-      ],
-      otherState: personsState.otherState  
+      ]  
     })
-  };
+  }
 
-  return (
-  <div className="App">
-    <h1>Hi, I'm a React App</h1>
-    <p>This is really working!</p>
-    <button onClick={ switchNameHandler }>Switch Name</button>
-    <Person name={ personsState.persons[0].name } age={ personsState.persons[0].age }/>
-    <Person name={ personsState.persons[1].name } age={ personsState.persons[1].age }>Hobbies: like dogs</Person>
-    <Person name={ personsState.persons[2].name } age={ personsState.persons[2].age }/>
-  </div>
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: 'Staszek', age: 36 },
+        { name: event.target.value, age: 35 },
+        { name: 'Pamirek', age: 7 }
+      ]  
+    })
+  }
+
+  render () {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
+    return (
+    <div className="App">
+     <h1>Hi, I'm a React App</h1>
+     <p>This is really working!</p>
+     <button 
+        //inline style
+        style={ style }
+        onClick={() => this.switchNameHandler('Stanislaw') }>Switch Name</button>
+     <Person 
+        name={ this.state.persons[0].name } 
+        age={ this.state.persons[0].age }/>
+     <Person 
+        name={ this.state.persons[1].name } 
+        age={ this.state.persons[1].age }
+        //Note: bind is more efficient then the arrow function in button
+        click={ this.switchNameHandler.bind(this, 'Stachu') }
+        changed={this.nameChangedHandler} >Hobbies: like dogs</Person>
+     <Person 
+        name={ this.state.persons[2].name } 
+        age={ this.state.persons[2].age }/>
+    </div>
   );
-//above is same as:
-//return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App' ));
-}
+  //above is same as:
+  //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App' ));
+  }
 
+}
 
 export default App;
