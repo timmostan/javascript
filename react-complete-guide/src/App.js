e54c4a08d154a48ec14cdab2f91db4ed40058457
timmostan <timmo@poczta.fm>
2020-02-23 12:08:54 +0100

    Better way for conditional content

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -49,17 +49,11 @@ class App extends Component {
       cursor: 'pointer'
     };
 
-    return (
-    <div className="App">
-     <h1>Hi, I'm a React App</h1>
-     <p>This is really working!</p>
-     <button 
-        //inline style
-        style={ style }
-        onClick={ this.togglePersonsHandler }>Switch Name</button>
-     { //turnery operator example in JSX - cannot use if here
-      this.state.showPersons ?
-      <div>
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
           <Person 
               name={ this.state.persons[0].name } 
               age={ this.state.persons[0].age }/>
@@ -72,8 +66,19 @@ class App extends Component {
           <Person 
               name={ this.state.persons[2].name } 
               age={ this.state.persons[2].age }/>
-        </div> : null
-      }
+        </div> 
+      );
+    }
+
+    return (
+    <div className="App">
+     <h1>Hi, I'm a React App</h1>
+     <p>This is really working!</p>
+     <button 
+        //inline style
+        style={ style }
+        onClick={ this.togglePersonsHandler }>Toggle persons</button>
+        { persons }
     </div>
   );
   //above is same as:
